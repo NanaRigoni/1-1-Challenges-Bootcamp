63e5f00260395a1234faa11ae21dc89e6b4467fb
Eliana Rigoni <62025867+NanaRigoni@users.noreply.github.com>
2020-04-22 18:22:20 -0300

    add desafio 1.2

---
+++ 1-2desempresa.js
@@ -0,0 +1,11 @@
+const empresa = {
+    nome: "Rocketseat",
+    cor: "Roxo",
+    foco: "Programação",
+    endereco: "Rua Guilherme Gembala, nº 260",
+}
+console.log(`A empresa ${empresa.nome} está localizada em ${empresa.endereco}`)
+
+
+
+
